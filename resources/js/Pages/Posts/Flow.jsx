b437c51727152deb7927050f7ae2523f8dbd762f
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2024-07-17 09:01:35 +0700

    penambahan fitur
    page controller, page home, about, dan flow

---
+++ resources/js/Pages/Posts/Flow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Flow() {
+  return (
+    <div>ubah ini</div>
+  )
+}
+
+export default Flow
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::get('/', [PagesController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
