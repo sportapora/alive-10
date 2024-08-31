e2d9265e95f99e5fe4c16615098a4f7800d5685d
thestrixy <gerardstefangani@gmail.com>
2024-08-31 15:26:08 +0700

    update
    mau condition

--- resources/js/Components/Home/LandingHome.jsx
+++ resources/js/Components/Home/LandingHome.jsx
@@ -2,45 +2,28 @@ import React from "react";
 import Ellipse from "../../../../public/Assets/Images/home_assets/Ellipse.png";
 import Hydro from "../../../../public/Assets/Images/home_assets/Copy of maskot 1-02.png";
 import Hydra from "../../../../public/Assets/Images/home_assets/Copy of maskot 2-03.png";
+import Maskot from "../../../../public/Assets/Images/home_assets/maskot merged.png";
+import Jeruk from "../../../../public/Assets/Images/home_assets/jeruk.png";
 
 export default function LandingHome() {
     return (
         <>
             <div className="bg-[#ED9947] h-screen md:p-0 p-10 overflow-hidden">
-                <div className="grid md:grid-cols-2 gap-4 relative items-center top-20 md:top-0 translateY-[-50%] md:left-20">
+                <div className="grid md:grid-cols-2 gap-4 relative items-center top-20 md:top-[30%] translateY-[-50%] md:left-20">
                     <div className="w-[100%] font-jost text-white text-center md:text-left">
                         <h2 className="text-2xl md:text-5xl">Welcome to</h2>
                         <h1 className="font-modak text-[50px] text-8xl md:text-[170px]">
                             ALIVE 10.0
                         </h1>
                         <h2 className="text-xl md:text-2xl">
-                            <b>Harmonize </b>your skin <b>confidence</b> within
+                            <b>Harmonize </b>Your Skin <b>Confidence</b> Within
                         </h2>
                     </div>
-                    <div className="grid relative items-center justify-center">
-                        <img src={Ellipse} className="w-[305px] md:w-[710px]" />
-                        <div className="absolute left-0 right-0 grid grid-cols-2 justify-center">
-                            <img
-                                src={Hydro}
-                                className="w-[305px] md:w-[710px]"
-                            />
-                            <img
-                                src={Hydra}
-                                className="w-[305px] md:w-[710px]"
-                            />
+                    <div className="grid relative items-center justify-center z-10">
+                        <img src={Ellipse} className="w-[305px] md:w-[400px]" />
+                        <div className="absolute left-0 right-0 grid grid-cols-1 justify-center">
+                            <img src={Maskot} />
                         </div>
-                    </div>
-                    <div>
-                        <svg
-                            viewBox="0 0 500 500"
-                            preserveAspectRatio="xMinYMin meet"
-                            className="relative w-screen "
-                        >
-                            <path
-                                d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z"
-                                className="bg-[#ED9947]"
-                            ></path>
-                        </svg>
                     </div>
                 </div>
             </div>
--- resources/js/Pages/Posts/Home.jsx
+++ resources/js/Pages/Posts/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Navbar from '@/Components/SmallComp/Navbar'
-import LandingHome from '@/Components/Home/LandingHome'
+import React from "react";
+import Navbar from "@/Components/SmallComp/Navbar";
+import LandingHome from "@/Components/Home/LandingHome";
+import AboutHome from "@/Components/Home/AboutHome";
 
 function Home() {
-  return (
-    <div>
-      <Navbar/>
-      <LandingHome/>
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <LandingHome />
+            <AboutHome />
+        </div>
+    );
 }
 
-export default Home
+export default Home;
