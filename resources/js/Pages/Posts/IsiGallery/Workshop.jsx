7a9db8e6cd55b7d7288b8d548c7c2f006a406ef4
MichKyleEl <164639246+MichKyleEl@users.noreply.github.com>
2024-09-14 15:49:33 +0700

    Tambahin Page Gallery(link dalem page masi error)

---
+++ resources/js/Pages/Posts/IsiGallery/Workshop.jsx
@@ -0,0 +1,20 @@
+import Navbar from '@/Components/SmallComp/Navbar'
+import Footer from '@/Components/SmallComp/Footer'
+import React from 'react'
+import {Head} from "@inertiajs/react";
+import Introduction from '@/Components/Gallery/Introduction'
+
+function Gallery() {
+  return (
+    <>
+        <Head title="Gallery" />
+      <div>
+        <Navbar />
+        <Introduction theme="dark" bgimg="shelf"/>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Gallery
