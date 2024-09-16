09317361bb8cbbd2e4b78f4ddd78e5ee3263fe7d
MichKyleEl <164639246+MichKyleEl@users.noreply.github.com>
2024-09-16 11:23:46 +0700

    Add files via upload
    navbar jadi responsive + blur

--- resources/js/Components/SmallComp/Navbar.jsx
+++ resources/js/Components/SmallComp/Navbar.jsx
@@ -9,15 +9,16 @@ function Navbar() {
         { name: "HOME", link: "/" },
         { name: "ABOUT", link: "/about" },
         { name: "FLOW", link: "/flow" },
+        { name: "GALLERY", link: "/gallery" },
         {
-            name: "GALLERY",
+            name: "",
             subLinks: [
                 { name: "Rapat Pleno 1", link: "/gallery/pleno1" },
                 { name: "Rapat Pleno 2", link: "/gallery/pleno2" },
-                { name: "Rapat Pleno 2", link: "/gallery/dokumentasi" },
-                { name: "Booth", link: "/gallery/dokumentasi" },
-                { name: "Workshop", link: "/gallery/dokumentasi" },
-                { name: "Talkshow", link: "/gallery/dokumentasi" },
+                { name: "Rapat Pleno 3", link: "/gallery/pleno3" },
+                { name: "Booth", link: "/gallery/booth" },
+                { name: "Workshop", link: "/gallery/workshop" },
+                { name: "Talkshow", link: "/gallery/talkshow" },
             ],
         },
     ];
@@ -32,10 +33,10 @@ function Navbar() {
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0 || open) {
-                setNavBackground("bg-[#C97A40] bg-opacity-60");
+                setNavBackground("bg-[#C97A40] bg-opacity-60 backdrop-blur-md");
                 setTextColor("text-white");
             } else {
-                setNavBackground("bg-transparent");
+                setNavBackground("bg-transparent backdrop-blur-none");
                 setTextColor("text-black");
             }
         };
@@ -61,51 +62,47 @@ function Navbar() {
         <div
             className={`w-[100%] fixed top-0 left-0 z-50 ${navBackground} transition-colors duration-300`}
         >
-            <div className="md:flex items-center justify-between py-1 md:px-10 px-7">
+            <div className="lg:flex items-center justify-between py-1 lg:px-10 px-7">
                 <div
                     className={`font-bold text-2xl cursor-pointer flex items-center ${textColor} font-sans`}
                 >
-                    <span className="text-3xl flex flex-row md:items-center items-center gap-10 mr-4 md:pt-0 pt-2">
+                    <span className="text-3xl flex flex-row lg:items-center items-center gap-10 mr-4 lg:pt-0 pt-2">
                         <img
                             src={Medic}
                             alt="logo_medic"
-                            className="md:h-40 w-auto h-20"
+                            className="lg:h-40 w-auto h-20"
                         />
                         <img
                             src={Alive}
                             alt="logo_alive"
-                            className="md:h-20 w-auto h-10"
+                            className="lg:h-20 w-auto h-10"
                         />
                     </span>
                 </div>
                 <div
                     onClick={() => setOpen(!open)}
-                    className={`text-3xl absolute right-8 top-9 cursor-pointer md:hidden ${textColor}`}
+                    className={`text-3xl absolute right-8 top-9 cursor-pointer lg:hidden ${textColor}`}
                 >
                     <IonIcon name={open ? "close" : "menu"}></IonIcon>
                 </div>
                 <ul
-                    className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0
-            w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
+                    className={`lg:flex lg:items-center lg:pb-0 pb-12 absolute lg:static lg:z-auto z-[-1] left-0
+            w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in 
             ${
                 open
                     ? `top-[96px] ${navBackground}`
                     : "top-[-490px] bg-transparent"
             } ${textColor}`}
                 >
-                    <li className="md:ml-8 mr-6 md:my-0 my-7 text-center">
+                    <li className="lg:ml-8 mr-6 lg:my-0 my-7 text-center">
                         <Link href={Links[0].link} className="hover:text-white">
-                            {/* <button
-                                className={`rounded-full px-5 py-1 focus:outline-none focus:ring-4 focus:ring-blue-300 ${textColor}`}
-                            > */}
                             {Links[0].name}
-                            {/* </button> */}
                         </Link>
                     </li>
                     {Links.slice(1).map((link, index) => (
                         <li
                             key={link.name}
-                            className={`md:ml-8 text-[16px] hover:text-white md:my-0 mr-6 my-7 text-center ${textColor}`}
+                            className={`lg:ml-8 text-[16px] hover:text-white lg:my-0 mr-6 my-7 text-center ${textColor}`}
                         >
                             {link.subLinks ? (
                                 <>
@@ -138,9 +135,6 @@ function Navbar() {
                                                     <Link href={subLink.link}>
                                                         {subLink.name}
                                                     </Link>
-                                                    {/* <a href={subLink.link}>
-                                                      {subLink.name}
-                                                  </a> */}
                                                 </li>
                                             )
                                         )}
@@ -148,7 +142,6 @@ function Navbar() {
                                 </>
                             ) : (
                                 <Link href={link.link}>{link.name}</Link>
-                                // <a href={Link.link}>{Link.name}</a>
                             )}
                         </li>
                     ))}
