081461bdff0e80a83e3ea37afda5375a14cb2449
MichKyleEl <164639246+MichKyleEl@users.noreply.github.com>
2024-09-19 14:35:20 +0700

    Squashed commit of the following:
    commit 99d873dcc79273c93f4aea75d6e731910a865e7c
    Merge: 103f867 c0a44ed
    Author: Henry Salim <78518644+sportapora@users.noreply.github.com>
    Date:   Thu Sep 19 14:19:33 2024 +0700
    
        Merge pull request #13 from sportapora/henry/wording-flow
    
        added flow description; fix footer gap; fix gallery footer
    
    commit c0a44edcf78536b5894de10ce9e23bcdbe8eaf1f
    Author: sportapora <henrysalim22@gmail.com>
    Date:   Thu Sep 19 14:18:42 2024 +0700
    
        added flow description; fix footer gap; fix gallery footer
    
    commit 103f867cda08eb6935a9280bea6afe8b0b85d43e
    Merge: 9d4dc10 a7bac6c
    Author: Henry Salim <78518644+sportapora@users.noreply.github.com>
    Date:   Wed Sep 18 17:14:16 2024 +0700
    
        Merge pull request #12 from sportapora/michael/gallery
    
        Michael/gallery
    
    commit 9d4dc106de7eb50778fe53fecd0aa0bda7495c6b
    Merge: cbd6635 479aaa2
    Author: Henry Salim <78518644+sportapora@users.noreply.github.com>
    Date:   Wed Sep 18 17:13:18 2024 +0700
    
        Merge pull request #11 from sportapora/michael/gallery
    
        Michael/gallery

---
+++ resources/js/Components/SmallComp/FooterGallery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+// ngetes pake footer doang (template)
+const FooterGallery = () => {
+    return (
+        <footer className='bg-[#ed9948] p-10'>
+            <h1 className='text-[#6B4E76] font-extrabold text-[20px] text-center'>
+                © 2024 Alive Medic UMN All Rights Reserved.
+            </h1>
+        </footer>
+    );
+};
+
+export default FooterGallery;
