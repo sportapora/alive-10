95c72107d6848016829c80bff37ac1316671fb32
rVerKotz <savero2605@gmail.com>
2024-09-24 19:12:30 +0700

    Update logo gambar filosofi

--- resources/js/Components/Home/Filosofi.jsx
+++ resources/js/Components/Home/Filosofi.jsx
@@ -5,32 +5,45 @@ const LogoPhilosophy = () => {
         <div className="bg-[#2B2B2B] text-white p-6">
             <h2 className="text-center text-2xl mb-6">Logo Philosophy</h2>
             <div className="flex md:flex-row flex-col gap-4 justify-around items-center">
-                <div className="bg-gray-200 w-[40%] h-52 flex flex-col items-center justify-center rounded-lg">
-                    <div
-                        className="w-12 h-12 bg-contain bg-no-repeat mb-4"
-                        style={{
-                            backgroundImage: `url("Assets/Images/home_assets/ARM.png")`,
-                        }}
-                    ></div>
-                    <p className="text-lg text-gray-800">Arm</p>
+                {/* Card 1 */}
+                <div className="relative bg-gray-200 w-[40%] h-52 flex flex-col items-center justify-center rounded-lg shadow-lg">
+                    {/* Circular Icon */}
+                    <div className="absolute -top-6 w-16 h-16 bg-white rounded-full flex items-center justify-center border border-gray-300 shadow-md">
+                        <div
+                            className="w-12 h-12 bg-contain bg-no-repeat"
+                            style={{
+                                backgroundImage: `url("Assets/Images/home_assets/ARM.png")`,
+                            }}
+                        ></div>
+                    </div>
+                    {/* Card Text */}
+                    <p className="mt-10 text-lg text-gray-800">Arm</p>
                 </div>
-                <div className="bg-gray-200 w-[40%] h-52 flex flex-col items-center justify-center rounded-lg">
-                    <div
-                        className="w-12 h-12 bg-contain bg-no-repeat mb-4"
-                        style={{
-                            backgroundImage: `url("Assets/Images/home_assets/HEART.png")`,
-                        }}
-                    ></div>
-                    <p className="text-lg text-gray-800">Heart</p>
+
+                {/* Card 2 */}
+                <div className="relative bg-gray-200 w-[40%] h-52 flex flex-col items-center justify-center rounded-lg shadow-lg">
+                    <div className="absolute -top-6 w-16 h-16 bg-white rounded-full flex items-center justify-center border border-gray-300 shadow-md">
+                        <div
+                            className="w-12 h-12 bg-contain bg-no-repeat"
+                            style={{
+                                backgroundImage: `url("Assets/Images/home_assets/HEART.png")`,
+                            }}
+                        ></div>
+                    </div>
+                    <p className="mt-10 text-lg text-gray-800">Heart</p>
                 </div>
-                <div className="bg-gray-200 w-[40%] h-52 flex flex-col items-center justify-center rounded-lg">
-                    <div
-                        className="w-12 h-12 bg-contain bg-no-repeat mb-4"
-                        style={{
-                            backgroundImage: `url("Assets/Images/home_assets/CROSS.png")`,
-                        }}
-                    ></div>
-                    <p className="text-lg text-gray-800">Cross</p>
+
+                {/* Card 3 */}
+                <div className="relative bg-gray-200 w-[40%] h-52 flex flex-col items-center justify-center rounded-lg shadow-lg">
+                    <div className="absolute -top-6 w-16 h-16 bg-white rounded-full flex items-center justify-center border border-gray-300 shadow-md">
+                        <div
+                            className="w-12 h-12 bg-contain bg-no-repeat"
+                            style={{
+                                backgroundImage: `url("Assets/Images/home_assets/CROSS.png")`,
+                            }}
+                        ></div>
+                    </div>
+                    <p className="mt-10 text-lg text-gray-800">Cross</p>
                 </div>
             </div>
         </div>
