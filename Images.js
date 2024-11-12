f9562df5f131cdab727f66feeb635b34080d0972
MichKyleEl <164639246+MichKyleEl@users.noreply.github.com>
2024-11-12 17:01:13 +0700

    Add files via upload

---
+++ Images.js
@@ -0,0 +1,38 @@
+const Images = [
+    {
+        category: "Pleno 1",
+        images: [
+            "pleno1/1.JPG",
+            "pleno1/2.JPG",
+            "pleno1/3.JPG",
+            "pleno1/4.JPG",
+            "pleno1/5.JPG",
+            "pleno1/6.JPG",
+            "pleno1/7.JPG",
+            "pleno1/8.JPG",
+            "pleno1/9.JPG",
+            "pleno1/10.JPG",
+            "pleno1/11.JPG",
+            "pleno1/12.JPG",
+        ],
+    },
+    {
+        category: "Pleno 2",
+        images: [
+            "pleno2/1.jpg",
+            "pleno2/2.jpg",
+            "pleno2/3.jpg",
+            "pleno2/4.jpg",
+            "pleno2/5.jpg",
+            "pleno2/6.jpg",
+            "pleno2/7.jpg",
+            "pleno2/8.jpg",
+            "pleno2/9.jpg",
+            "pleno2/10.jpg",
+            "pleno2/11.jpg",
+            "pleno2/12.jpg",
+        ]
+    },
+];
+
+export default Images;
